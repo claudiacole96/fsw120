14b693ed12e58d78d6a0a0dde8faf75dae302dda
claudiacole96 <claudia.evans@bryanuniversity.edu>
2021-01-07 20:38:14 -0500

    week2

--- w2-colored-info-boxes/src/App.js
+++ w2-colored-info-boxes/src/App.js
@@ -5,16 +5,17 @@ import Block from "./components/Block"
 function App() {
   return (
     <div>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block title="box" subtitle="something" info="red box"/>
+      <h1>Top 10 TV Shows</h1>
+      <Block title="Planet Earth ll" subtitle="(2016)" info="Rating: 9.5" backgroundColor="GoldenRod"/>
+      <Block title="Planet Earth" subtitle="(2006)" info="Rating: 9.4" backgroundColor="Gray"/>
+      <Block title="Band of Brothers" subtitle="(2001)" info="Rating: 9.4" backgroundColor="DarkGoldenRod"/>
+      <Block title="Breaking Bad" subtitle="(2008)" info="Rating: 9.4" backgroundColor="LightBlue"/>
+      <Block title="Chernobyl" subtitle="(2019)" info="Rating: 9.4" backgroundColor="Purple"/>
+      <Block title="The Wire" subtitle="(2002)" info="Rating: 9.3" backgroundColor="Blue"/>
+      <Block title="Blue Planet ll" subtitle="(2017)" info="Rating: 9.3" backgroundColor="Green"/>
+      <Block title="Our Planet" subtitle="(2019)" info="Rating: 9.3" backgroundColor="Yellow"/>
+      <Block title="Cosmos: A Spacetime Odyssey" subtitle="(2014)" info="Rating: 9.2" backgroundColor="Orange"/>
+      <Block title="Cosmos" subtitle="(1980)" info="Rating: 9.2" backgroundColor="Red"/>
     </div>
   );
 }
--- w2-colored-info-boxes/src/components/Block.js
+++ w2-colored-info-boxes/src/components/Block.js
@@ -2,10 +2,12 @@ import React from "react"
 
 function Block(props){
     return (
-        <div>
+        <div style={{backgroundColor: props.backgroundColor, padding: "10px", marginBottom: "10px"}}>
             <h1>{props.title}</h1>
             <h3>{props.subtitle}</h3>
             <p>{props.info}</p>
         </div>
     )
 }
+
+export default Block
